323a5a405341ae399317615aefdfe09d32f813aa
George Higbie <ghigbie@monigle.com>
2019-03-28 08:25:39 -0600

    Edit xpath for each var

--- quotes_spider/spiders/quotes.py
+++ quotes_spider/spiders/quotes.py
@@ -11,11 +11,12 @@ class QuotesSpider(scrapy.Spider):
         # h1_tag = response.xpath('//h1/a/text()').extract_first()
         # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract_first
         # yield {'H1 Tag': h1_tag, 'Tags': tags}
-        quotes = response.xpath('//*[@class="quote]')
+        quotes = response.xpath('.//*[@class="quote"]')
         for quote in quotes:
             text = quote.xpath('.//*[@class="text"]/text()').extract()
-            author = quote.xpath('.//*[@itemprop="author]/text()').extract()
-            tags = quote.xpath('.//*[@itemprop="keywords]/@content').extract()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract()
+
             print('\n')
             print(text)
             print(author)
