9186d07c011af06756c27f74acea683c40eb6984
George Higbie <ghigbie@monigle.com>
2019-03-28 08:28:52 -0600

    Edit spider output

--- quotes_spider/spiders/quotes.py
+++ quotes_spider/spiders/quotes.py
@@ -13,9 +13,9 @@ class QuotesSpider(scrapy.Spider):
         # yield {'H1 Tag': h1_tag, 'Tags': tags}
         quotes = response.xpath('.//*[@class="quote"]')
         for quote in quotes:
-            text = quote.xpath('.//*[@class="text"]/text()').extract()
-            author = quote.xpath('.//*[@itemprop="author"]/text()').extract()
-            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract()
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
 
             print('\n')
             print(text)
